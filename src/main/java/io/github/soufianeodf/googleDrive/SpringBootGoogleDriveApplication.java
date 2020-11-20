3ff77cc4faf5daad17a8e600907a17fcdde42da4
soufianeodf <soufiane.odf@gmail.com>
2020-11-21 00:13:26 +0100

    Initialize bare spring boot rest project

---
+++ src/main/java/io/github/soufianeodf/googleDrive/SpringBootGoogleDriveApplication.java
@@ -0,0 +1,13 @@
+package io.github.soufianeodf.googleDrive;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootGoogleDriveApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootGoogleDriveApplication.class, args);
+    }
+
+}
---
+++ src/test/java/io/github/soufianeodf/googleDrive/SpringBootGoogleDriveApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.soufianeodf.googleDrive;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootGoogleDriveApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
