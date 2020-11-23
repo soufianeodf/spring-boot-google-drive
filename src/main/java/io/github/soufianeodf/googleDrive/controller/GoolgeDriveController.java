c220efc73d9c23c389755c5bdb874982c6359d89
soufianeodf <soufiane.odf@gmail.com>
2020-11-23 02:12:11 +0100

    Change structure of google drive service

--- src/main/java/io/github/soufianeodf/googleDrive/controller/GoolgeDriveController.java
+++ src/main/java/io/github/soufianeodf/googleDrive/controller/GoolgeDriveController.java
@@ -4,6 +4,8 @@ import io.github.soufianeodf.googleDrive.service.GoogleDriveService;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.security.GeneralSecurityException;
 
@@ -17,6 +19,21 @@ public class GoolgeDriveController {
     }
 
     @GetMapping("/")
+    public String showHomePage() throws Exception {
+        return googleDriveService.showHomePage();
+    }
+
+    @GetMapping(value = { "/googlesignin" })
+    public void doGoogleSignIn(HttpServletResponse response) throws Exception {
+        googleDriveService.doGoogleSignIn(response);
+    }
+
+    @GetMapping(value = { "/oauth" })
+    public String saveAuthorizationCode(HttpServletRequest request) throws Exception {
+        return googleDriveService.saveAuthorizationCode(request);
+    }
+
+    @GetMapping("/main")
     public void main() throws IOException, GeneralSecurityException {
         googleDriveService.main("1ZZWExvIj9t0eHzCr-z3uAXUEHwFt9DnefId2DabecDU");
     }
--- src/main/java/io/github/soufianeodf/googleDrive/service/GoogleDriveService.java
+++ src/main/java/io/github/soufianeodf/googleDrive/service/GoogleDriveService.java
@@ -4,9 +4,12 @@ import com.google.api.client.auth.oauth2.Credential;
 import com.google.api.client.extensions.java6.auth.oauth2.AuthorizationCodeInstalledApp;
 import com.google.api.client.extensions.jetty.auth.oauth2.LocalServerReceiver;
 import com.google.api.client.googleapis.auth.oauth2.GoogleAuthorizationCodeFlow;
+import com.google.api.client.googleapis.auth.oauth2.GoogleAuthorizationCodeRequestUrl;
 import com.google.api.client.googleapis.auth.oauth2.GoogleClientSecrets;
+import com.google.api.client.googleapis.auth.oauth2.GoogleTokenResponse;
 import com.google.api.client.googleapis.javanet.GoogleNetHttpTransport;
 import com.google.api.client.http.FileContent;
+import com.google.api.client.http.HttpTransport;
 import com.google.api.client.http.javanet.NetHttpTransport;
 import com.google.api.client.json.JsonFactory;
 import com.google.api.client.json.jackson2.JacksonFactory;
@@ -18,6 +21,9 @@ import com.google.api.services.drive.model.FileList;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 
+import javax.annotation.PostConstruct;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 import java.io.*;
 import java.security.GeneralSecurityException;
 import java.util.Collections;
@@ -26,12 +32,14 @@ import java.util.List;
 @Service
 public class GoogleDriveService {
 
-    private final Drive service;
-    private final NetHttpTransport HTTP_TRANSPORT;
+    private static final String CALLBACK_URI = "http://localhost:8081/oauth";
+    private final HttpTransport HTTP_TRANSPORT = new NetHttpTransport();;
 
     private static final String APPLICATION_NAME = "Google Drive API Java Quickstart";
     private static final JsonFactory JSON_FACTORY = JacksonFactory.getDefaultInstance();
     private static final String TOKENS_DIRECTORY_PATH = "tokens";
+    private static final String USER_IDENTIFIER_KEY = "MY_DUMMY_USER";
+    private static GoogleAuthorizationCodeFlow flow;
 
     /**
      * Global instance of the scopes required by this quickstart.
@@ -40,21 +48,11 @@ public class GoogleDriveService {
     private static final List<String> SCOPES = Collections.singletonList(DriveScopes.DRIVE);
     private static final String CREDENTIALS_FILE_PATH = "/credentials.json";
 
-    public GoogleDriveService() throws GeneralSecurityException, IOException {
-        // Build a new authorized API client service.
-        HTTP_TRANSPORT = GoogleNetHttpTransport.newTrustedTransport();
-        service = new Drive.Builder(HTTP_TRANSPORT, JSON_FACTORY, getCredentials(HTTP_TRANSPORT))
-                .setApplicationName(APPLICATION_NAME)
-                .build();
+    public GoogleDriveService() {
     }
 
-    /**
-     * Creates an authorized Credential object.
-     * @param HTTP_TRANSPORT The network HTTP Transport.
-     * @return An authorized Credential object.
-     * @throws IOException If the credentials.json file cannot be found.
-     */
-    private static Credential getCredentials(final NetHttpTransport HTTP_TRANSPORT) throws IOException {
+    @PostConstruct
+    private void init() throws IOException {
         // Load client secrets.
         InputStream in = GoogleDriveService.class.getResourceAsStream(CREDENTIALS_FILE_PATH);
         if (in == null) {
@@ -63,16 +61,25 @@ public class GoogleDriveService {
         GoogleClientSecrets clientSecrets = GoogleClientSecrets.load(JSON_FACTORY, new InputStreamReader(in));
 
         // Build flow and trigger user authorization request.
-        GoogleAuthorizationCodeFlow flow = new GoogleAuthorizationCodeFlow.Builder(
+        flow = new GoogleAuthorizationCodeFlow.Builder(
                 HTTP_TRANSPORT, JSON_FACTORY, clientSecrets, SCOPES)
                 .setDataStoreFactory(new FileDataStoreFactory(new java.io.File(TOKENS_DIRECTORY_PATH)))
                 .setAccessType("offline")
                 .build();
-        LocalServerReceiver receiver = new LocalServerReceiver.Builder().setPort(8888).build();
-        return new AuthorizationCodeInstalledApp(flow, receiver).authorize("user");
     }
 
     public void main(String fileId) {
+        // Build a new authorized API client service.
+        Credential cred = null;
+        try {
+            cred = flow.loadCredential(USER_IDENTIFIER_KEY);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        Drive service = new Drive.Builder(HTTP_TRANSPORT, JSON_FACTORY, cred)
+                .setApplicationName(APPLICATION_NAME).build();
+
         // get file
         File file = null;
         try {
@@ -80,6 +87,8 @@ public class GoogleDriveService {
         } catch (IOException e) {
             e.printStackTrace();
         }
+
+        System.out.println("the file id is: " + file.getId());
 
         // copy the file
         File fileCopy = null;
@@ -148,5 +157,60 @@ public class GoogleDriveService {
         } else {
             System.out.println("Failed to delete the file.");
         }
+    }
+
+    public String showHomePage() {
+        boolean isUserAuthenticated = false;
+
+        Credential credential = null;
+        try {
+            credential = flow.loadCredential(USER_IDENTIFIER_KEY);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        if (credential != null) {
+            boolean tokenValid = false;
+            try {
+                tokenValid = credential.refreshToken();
+                if (tokenValid) {
+                    isUserAuthenticated = true;
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+
+        return isUserAuthenticated ? "you are logged in" : "sorry, you need to log in first. Go to /googlesignin";
+    }
+
+    public void doGoogleSignIn(HttpServletResponse response) {
+        GoogleAuthorizationCodeRequestUrl url = flow.newAuthorizationUrl();
+        String redirectURL = url.setRedirectUri(CALLBACK_URI).setAccessType("offline").build();
+        try {
+            response.sendRedirect(redirectURL);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public String saveAuthorizationCode(HttpServletRequest request) {
+        String code = request.getParameter("code");
+        if (code != null) {
+            try {
+                saveToken(code);
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+
+            return "dashboard.html";
+        }
+
+        return "index.html";
+    }
+
+    private void saveToken(String code) throws Exception {
+        GoogleTokenResponse response = flow.newTokenRequest(code).setRedirectUri(CALLBACK_URI).execute();
+        flow.createAndStoreCredential(response, USER_IDENTIFIER_KEY);
     }
 }
